838992feeb26df11efa2988880ccc0583c4b2f69
Andrii Popov <popov.andrey80@gmail.com>
2022-12-03 22:11:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import { data, user, friends } from '../data/index';
@@ -6,7 +5,7 @@ import { data, user, friends } from '../data/index';
 export const App = () => {
   return (
     <div>
-      <Profile user={user} />
+      {/* <Profile user={user} /> */}
       <Statistics stats={data} title={'Upload stats'} />
       <FriendList friends={friends} />
     </div>
