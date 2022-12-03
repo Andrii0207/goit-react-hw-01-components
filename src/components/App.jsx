3f4a4307dab34f07f15c15e80b1c86905074d5d6
Andrii Popov <popov.andrey80@gmail.com>
2022-12-03 21:40:15 +0200

    task-03 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import { data, user } from '../data/index';
+import FriendList from './FriendList/FriendList';
+import { data, user, friends } from '../data/index';
 
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
       <Statistics stats={data} title={'Upload stats'} />
+      <FriendList friends={friends} />
     </div>
   );
 };
