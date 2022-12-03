b8b180efad1f9024052da4210c37c9546d72ff39
Andrii Popov <popov.andrey80@gmail.com>
2022-12-03 19:08:50 +0200

    task-02 done!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import Profile from './Profile/Profile';
 import user from './Profile/user.json';
-import StatList from './Statistics/Statistics';
+// import StatList from './Statistics/Statistics';
+import Statistics from './Statistics/Statistics';
 import data from './Statistics/data.json';
 
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
-      <StatList label={data.label} percentage={data.percentage} />
+      {/* <StatList label={data.label} percentage={data.percentage} /> */}
+      <Statistics stats={data} />
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types';
+import { Stats } from './Stats';
 
-function Statistics({ title }) {
-  return <section>{title && <h2>{title}</h2>}</section>;
+function Statistics({ title, stats }) {
+  return (
+    <section>
+      {title && <h2>{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li className="item" key={id}>
+            <Stats label={label} percentage={percentage} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 Statistics.propTypes = {
